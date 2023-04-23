bc09f8673a83093639a13920c158cafe146b6b55
EC2 Default User <ec2-user@ip-172-31-2-45.ap-northeast-1.compute.internal>
2023-04-23 07:24:48 +0000

    Update 一覧画面表示機能の追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
     #Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
     @list = List.new
   end
-  
+
   def create
     # 1. データを受け取り、受け取ったデータを元にインスタンスを作成する。
     list = List.new(list_params)
@@ -12,8 +12,15 @@ class ListsController < ApplicationController
     # 3. トップ画面へのリダイレクト
     redirect_to '/top'
   end
-  
+
   def index
+    # あらかじめControllerのアクションでインスタンス変数を指定しておくことで、viewファイル上でインスタンス変数に格納された情報を表示させることができます。
+    # コントローラーとモデルとデータベースの関係を思いだして、データベースをいじるのはモデルだからここでList.all(モデル名.all)としている。
+    # コントローラーがモデルの操作をするときは、モデル名.all にする。決してモデル名にｓをつけたりしない。上のcreateやnewでも同様。
+    # モデルからインスタンスを生成した時のインスタンス名は時と場合による。小文字始まりは絶対。クラスは大文字始まり。インスタンスは小文字始まり。
+    # 複数形のｓをつけるか否かはそのインスタンス（オブジェクト）が単体か複数かで考える。
+    # 上のcreateアクションの場合は単体だからlistになってる。一方でこのindexアクションではレコードが複数だからlistsになってる。
+    @lists = List.all
   end
 
   def show
@@ -21,11 +28,11 @@ class ListsController < ApplicationController
 
   def edit
   end
-  
+
   private
   # ストロングパラメータ
   # privateより後に定義されたメソッドは、アクションとして認識されなくなり、URLと対応できなくなります。
   def list_params
     params.require(:list).permit(:title, :body)
-  end 
+  end
 end
